12b99466beee49d9fa6fdda6d4f0d862ff2aa2b6
Duncan Neal <duncan.neal@gmail.com>
2015-05-31 18:14:13 -0400

    Completed App, no heroku yet

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  
+  http_basic_authenticate_with_name: "name", passsword: "password", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
